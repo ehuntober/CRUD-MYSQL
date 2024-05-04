d02e7dacf6b6f40c10e64fec89c76b5076ce7691
ehuntober <ehuntercrypto@gmail.com>
2024-05-04 14:19:22 +0100

    new file updated

--- client/index.js
+++ client/index.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
 function loadHTMLTable(data){
     const table = document.querySelector('table tbody');
-    let tableHtml = "";
     if(data.length === 0){
+        table.innerHTML = "<tr><td class='no-data' colspan='5'> No Data </td></tr>"
 
     }
 }
